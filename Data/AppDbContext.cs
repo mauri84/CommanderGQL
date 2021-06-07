044fd5c336a639bef26b0fab133edb2ff5b488ed
Mauricio Fleischer <mmfleischer@gmail.com>
2021-06-07 18:39:39 -0300

    Added DbContext to SQL server in Container

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using CommanderGQL.Models;
+
+namespace CommanderGQL.Data {
+  public class AppDbContext : DbContext {
+    public AppDbContext (DbContextOptions options) : base(options) { }
+
+    public DbSet<Platform> Platforms { get; set; }
+  }
+}
